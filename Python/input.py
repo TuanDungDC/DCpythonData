cc9b2d56c0b054f9f808f25ab4ffb1a0b1eb4ca4
ADKnguDC <adkrngu216@gmail.com>
2022-09-01 01:12:29 +0700

    update

--- Python/input.py
+++ Python/input.py
@@ -1,1 +1,6 @@
-a = input('Nhập mật khẩu của bạn : ')
+a = input('Nhập mật khẩu của bạn : ') # Đối với khi sử dụng lệnh input thì mặc định là kiểu dữ liệu string
+print(a)
+
+# Nên nếu muốn gán ngay từ khi nhập vào thì ta ép kiểu dữ liệu cho biến VD:
+b = int(input("Nhập giá trị của b : ")) # Ép biến b có kiểu dữ liệu là số nguyên int
+print(b + 4)
